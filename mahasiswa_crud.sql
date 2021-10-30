312edb1756c7ffbe3a32afc0c4063beb27c7dc24
Muhammad Agil Maulana <43842355+Mohanagil@users.noreply.github.com>
2021-10-30 18:32:38 +0700

    Add files via upload

---
+++ mahasiswa_crud.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 30, 2021 at 01:21 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mahasiswa_crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_mhs`
+--
+
+CREATE TABLE `tbl_mhs` (
+  `id` int(11) NOT NULL,
+  `nim` char(11) NOT NULL,
+  `namamhs` varchar(100) NOT NULL,
+  `jk` enum('L','P') NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `kota` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `foto` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_mhs`
+--
+
+INSERT INTO `tbl_mhs` (`id`, `nim`, `namamhs`, `jk`, `alamat`, `kota`, `email`, `foto`) VALUES
+(1, '20071283418', 'Mickey', 'L', 'Jl. Sukoharjo 5', 'Surabaya', 'ppmickey@lari.com', '813-wp9110624.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_mhs`
+--
+ALTER TABLE `tbl_mhs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_mhs`
+--
+ALTER TABLE `tbl_mhs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
